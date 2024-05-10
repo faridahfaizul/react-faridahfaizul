88c8d9324d42e22f05e578fef7858acc4129ee40
faridah faizul <fsyahirahfaizul@gmail.com>
2024-05-11 00:21:56 +0800

    Update project section

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ export default function Home() {
     <div className='container'>
         <div className='small-device'>
         <Row>
-            <Col sm={4}>
+            <Col sm={1}></Col>
+            <Col sm={3}>
                 <div className='fixed'> 
                     <Image src={'./images/me.jpg'} roundedCircle className='center-item profile-image' alt='My Image' />                         
                     <div className='small-hidden'>
@@ -42,7 +43,7 @@ export default function Home() {
             <Col sm={1}></Col>      
             <Col sm={7}>
                 <div>                        
-                    <h1 id="about" className='page-title'>hello, i'm Faridah</h1>
+                    <h1 id='about' className='page-title'>hello, i'm Faridah</h1>
                     <div className='profile-desc'>
                         <p>
                             I specialize in crafting <strong>Front-End</strong> websites as a 
@@ -121,6 +122,92 @@ export default function Home() {
                     <p className='line' />
 
                     <p id='projects' className='section-title'>Projects</p>
+                    <div className='grid'> 
+                        <div className='grid__item'>
+                            <a href='https://faridahfaizul.netlify.app/' target='_blank' className='profile-link'>
+                                <div className='card'>
+                                    <Row>
+                                        <Col sm={4}>
+                                            <Image className='project-image' src='.\images\works\portfolio.png' alt='FaridahFaizul Portfolio' />
+                                        </Col>
+                                        <Col sm={7}>                    
+                                            <h1 className='project-name'>
+                                                faridahfaizul.netlify.app
+                                                <span> <Icon.BoxArrowInUpRight color='#983961' size={18}/></span>
+                                            </h1>
+                                            <p className='project-details'>An old portfolio responsive website</p>
+                                            <ul className='project-tech'>
+                                                <li className='project-tech-list'>HTML</li>
+                                                <li className='project-tech-list'>CSS</li>
+                                                <li className='project-tech-list'>Netlify</li>
+                                                <li className='project-tech-list'>Github</li>
+                                            </ul>      
+                                        </Col>
+                                    </Row>   
+                                </div>
+                            </a>
+                        </div>
+                        <div className='grid__item'>
+                            <a href='https://faridahfaizul.000webhostapp.com/' target='_blank' className='profile-link'>
+                                <div className='card'>
+                                    <Row>
+                                        <Col sm={4}>
+                                             <Image className='project-image' src='.\images\works\dentalclinic.png' alt='Dental Clinic' />
+                                        </Col>
+                                        <Col sm={7}>                    
+                                            <h1 className='project-name'>
+                                                DentalClinic project
+                                                <span> <Icon.BoxArrowInUpRight color='#983961' size={18}/></span>
+                                            </h1>
+                                            <p className='project-details'>
+                                                Build a dynamic website which consist of 
+                                                user-site and admin-site that I create targeting the dental industry. 
+                                                This project aims to have good data management where the data from this
+                                                project is received from its own database
+                                            </p>
+                                            <ul className='project-tech'>
+                                                <li className='project-tech-list'>HTML</li>
+                                                <li className='project-tech-list'>CSS</li>
+                                                <li className='project-tech-list'>Bootstrap</li>
+                                                <li className='project-tech-list'>Javascript</li>
+                                                <li className='project-tech-list'>PHP</li>
+                                                <li className='project-tech-list'>CodeIgniter</li>
+                                                <li className='project-tech-list'>Github</li>
+                                            </ul>      
+                                        </Col>
+                                    </Row>   
+                                </div>
+                            </a>
+                        </div>
+                         <div className='grid__item'>
+                            <a href='https://drive.google.com/file/d/1ZBswRq40xSUvfX8K8crMOBVnVDm9Dott/view' target='_blank' className='profile-link'>
+                                <div className='card'>
+                                    <Row>
+                                        <Col sm={4}>                                            
+                                             <Image className='project-image' src='.\images\works\diyfar.png' alt='DIYfAR' />
+
+                                        </Col>
+                                        <Col sm={7}>                    
+                                            <h1 className='project-name'>
+                                                DIYfAR Mobile Application
+                                                <span> <Icon.BoxArrowInUpRight color='#983961' size={18}/></span>
+                                            </h1>
+                                            <p className='project-details'>
+                                                Do-It-Yourself (DIY) furniture based on augmented reality approach 
+                                                (DIYfAR) is proposed to help user to build a furniture with the use of AR platform.
+                                                Thia application focus on using markerless system technique, and user can design 
+                                                the furniture that they intended to build, view it in real-time and receive information in 
+                                                order to build the furniture.</p>
+                                            <ul className='project-tech'>
+                                                <li className='project-tech-list'>Blender</li>
+                                                <li className='project-tech-list'>Unity</li>
+                                            </ul>      
+                                        </Col>
+                                    </Row>   
+                                </div>
+                            </a>
+                        </div>
+                    </div>        
                 </div>
             </Col>
         </Row>
